31e81d8fcf877cdde8916b1c13418e5cdc89a741
Gini Stanzi <a.stanzione14@gmail.com>
2023-01-30 21:00:07 +0000

    Updated event listener for button to create the fetch request based on the input value

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
 
 
 
-fetch("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5&appid=0801e00bf8b67d7e81fc68b412514283")
+let buttonSearch = document.querySelector("#search-button");
+
+buttonSearch.addEventListener("click", function(event){
+
+event.preventDefault()
+
+let userSearchCity = document.querySelector("#search-input").value
+
+fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${userSearchCity}&limit=5&appid=0801e00bf8b67d7e81fc68b412514283`)
 .then(response => response.json())
-.then(function(citySearch){
+.then(citySearch => {
 
     let firstCity = citySearch[0]
     console.log(firstCity.lat)
@@ -13,11 +21,12 @@ fetch("https://api.openweathermap.org/geo/1.0/direct?q=Dallas&limit=5&appid=0801
 
 })
 
-
-
 .then(response => response.json())
-.then(function(data){
+.then(data => {
 
     console.log(data)
 
 })
+
+}
+);
