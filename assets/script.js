a987c658f8f6e6a5d11e4844036d989b7f8991ad
Gini Stanzi <a.stanzione14@gmail.com>
2023-01-31 00:13:52 +0000

    Updated comments on code for easier viewing

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ let buttonSearch = document.querySelector("#search-button");
 
 });
 
-
+//Creating fetch request on click of Search button
 buttonSearch.addEventListener("click", function(event){
 
 event.preventDefault()
